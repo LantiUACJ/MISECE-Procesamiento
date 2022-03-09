e6e90556f29dd7a66269907eb490e0083d8be8e4
jaredolmosmb <jaredarturolmos@gmail.com>
2022-03-09 15:21:03 -0700

    add data to new database

--- Primerproyecto/Aplicacion1/views.py
+++ Primerproyecto/Aplicacion1/views.py
@@ -4,7 +4,7 @@ from django.template.loader import render_to_string
 from django.shortcuts import render
 from .models import ConceptS, DescriptionS, Synonyms, ConceptosNoEncontrados, ExtendedmaprefsetS, SynonymVotes
 from django.db.models import Q
-from api.models import TokensDiagnosticos, TokensProcedures, Descripciones_y_sinonimos
+from api.models import TokensDiagnosticos, TokensProcedures, Descripciones_y_sinonimos, TokensDiagnosticosYSinonimos
 from .servicios import generarRequest, normalize, validateJSON
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
@@ -100,8 +100,8 @@ def Preprocesamiento(la_frase):
 def InicioView(request):
 	#pacientes = Paciente.objects.all()
 	#recurso = 'PruebaPOS'
-	#recurso = 'TokensDiagnosticos'
-	recurso = 'nueva_tabla'
+	recurso = 'TokensDiagnosticos'
+	#recurso = 'nueva_tabla'
 	#recurso = 'nueva_tabla_sinonimos'
 
 	if (recurso == 'nueva_tabla_sinonimos'):
@@ -129,7 +129,7 @@ def InicioView(request):
 		concepto1 = ConceptS.objects.filter(active ="1") and ConceptS.objects.filter(category_id ="6")
 		print("concepto1.coun", concepto1.count())
 
-		for i in concepto1[20000:40000]:#115484
+		for i in concepto1[115000:115484]:#115484
 			desc = DescriptionS.objects.filter(conceptid = i.id)
 			for j in desc:
 				obj, created = Descripciones_y_sinonimos.objects.update_or_create(
@@ -307,9 +307,20 @@ def InicioView(request):
 
 			start_time = time.time()
 			concepto1 = ConceptS.objects.filter(active ="1") and ConceptS.objects.filter(category_id ="6")
-			#print("concepto1.coun", concepto1.count())
+			print("concepto1.coun", concepto1.count())
 
-			for i in concepto1[0:0]:
+			"""for i in concepto1[0:0]:#115484
+													desc = DescriptionS.objects.filter(conceptid = i.id)
+													for j in desc:
+														#print(j.term)
+														tokens = [t for t in j.term.split()]
+														#tokens = word_tokenize(j.term)
+														filt_tokens = [w.lower() for w in tokens if not w.lower() in stop_words]
+														for k in filt_tokens:
+															TokensDiagnosticos.objects.create(token=k.lower(), id_descripcion=j.id, largo_palabras_termino=len(filt_tokens))
+															descAceptadas.append([k.lower(), j.id, len(filt_tokens)])"""
+
+			for i in concepto1[100000:115484]:#115484
 				desc = DescriptionS.objects.filter(conceptid = i.id)
 				for j in desc:
 					#print(j.term)
@@ -317,20 +328,8 @@ def InicioView(request):
 					#tokens = word_tokenize(j.term)
 					filt_tokens = [w.lower() for w in tokens if not w.lower() in stop_words]
 					for k in filt_tokens:
-						TokensDiagnosticos.objects.create(token=k.lower(), id_descripcion=j.id, largo_palabras_termino=len(filt_tokens))
+						TokensDiagnosticosYSinonimos.objects.create(token=k.lower(), id_descripcion=j.id, largo_palabras_termino=len(filt_tokens))
 						descAceptadas.append([k.lower(), j.id, len(filt_tokens)])
-			
-
-			sinonimos = Synonyms.objects.all()
-			for s in sinonimos:
-				tokens2 = [t for t in s.term.split()]
-				filt_tokens2 = [w.lower() for w in tokens2 if not w.lower() in stop_words]
-				for k2 in filt_tokens2:
-					TokensDiagnosticos.objects.create(token=k2.lower(), id_descripcion="s"+str(s.id), largo_palabras_termino=len(filt_tokens2))
-
-
-
-
 
 			#for j in range(1):
 				#for i in descripciones[j*5000:(j+1)*5000-1]:
--- Primerproyecto/api/models.py
+++ Primerproyecto/api/models.py
@@ -7,6 +7,13 @@ class TokensDiagnosticos(models.Model):
     largo_palabras_termino = models.IntegerField()
 # Create your models here.
 
+class TokensDiagnosticosYSinonimos(models.Model):
+    #id = models.CharField(max_length=36, primary_key=True)
+    token = models.CharField(max_length=255)
+    id_descripcion = models.CharField(max_length=255)
+    largo_palabras_termino = models.IntegerField()
+
+
 class TokensDiagnosticosFrecuentes(models.Model):
     #id = models.CharField(max_length=36, primary_key=True)
     token = models.CharField(max_length=255)
