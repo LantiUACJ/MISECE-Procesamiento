78b0f6c2d4bda408395ab2a0ac207cb0eebaea9b
jaredolmosmb <jaredarturolmos@gmail.com>
2022-03-18 12:41:31 -0600

    set redirect

--- Primerproyecto/Primerproyecto/urls.py
+++ Primerproyecto/Primerproyecto/urls.py
@@ -15,9 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('apli/', include('Aplicacion1.urls')),
-    path('api/', include('api.urls'))
+    path('api/', include('api.urls')),
+    path('', lambda req: redirect('/api/')),
 ]
