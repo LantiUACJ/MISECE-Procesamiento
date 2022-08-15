95251b7a310f695a11f259f9eeb556020cede7a0
jaredolmosmb <jaredarturolmos@gmail.com>
2022-08-15 15:23:10 -0600

    change diagnosticReport to Condition verified

--- Procesamiento_SNOMED/api/urls.py
+++ Procesamiento_SNOMED/api/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
 	path('', views.apiOverview, name =  "api-overview"),
 	path('procesarSNOMED/Bundle', views.ProcesarBundleView, name =  "api-procesarBundleSNOMED"),
+	path('procesarSNOMED/Condition', views.ProcesarConditionView, name =  "api-procesarConditionSNOMED"),
 	path('procesarSNOMED/DiagnosticReport', views.ProcesarDiagnosticReportView, name =  "api-procesarDiagnosticReportSNOMED"),
 	path('procesarSNOMED/Medication', views.ProcesarMedicationView, name =  "api-procesarMedicationSNOMED"),
 	path('procesarSNOMED/MedicationAdministration', views.ProcesarMedicationAdministrationView, name =  "api-procesarMedicationAdministrationSNOMED"),
