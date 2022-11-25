a61202592ef56688e39c7634ed5f5d9aa2c53887
Syscore1 <32805386+Syscore1@users.noreply.github.com>
2022-11-25 10:16:57 -0600

    fix update script

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -4,4 +4,4 @@ cd /
 cd var/www/html/MISECE-Procesamiento/Procesamiento_SNOMED/Procesamiento_SNOMED/
 sudo rm settings.py
 echo removed 
-sudo aws s3 cp s3://s3procesamiento/settings.py /var/www/html/MISECE-Procesamiento/ > /dev/null 2> /dev/null < /dev/null &
+sudo aws s3 cp s3://s3procesamiento/settings.py /var/www/html/MISECE-Procesamiento/Procesamiento_SNOMED/Procesamiento_SNOMED/ > /dev/null 2> /dev/null < /dev/null &
